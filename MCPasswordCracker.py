2dfc9439f216f825b9d16ed9633047f101772262
Christie Lalande <chrislalande@Christies-MacBook-Pro.local>
2022-06-06 14:28:50 -0500

    Initial commit

---
+++ MCPasswordCracker.py
@@ -0,0 +1,48 @@
+# PASSWORD CRACKER DEMO
+# We have a file of the 1000 most common passwords that we will compare the password to
+# The hashed password we are cracking is 2b58af6dddbd072ed27ffc86725d7d3a
+
+# Load up an MD5 library
+import hashlib
+
+# Set the password we are trying to crack into a constant
+# By convention, constants are named with all caps
+# This password is a hashed hexadecimal equivalent of a string
+PASSWORD_TO_CRACK = "2b58af6dddbd072ed27ffc86725d7d3a"
+
+# Read in the file of example passwords
+password_file = open('10k-most-common.txt', 'r')
+common_passwords = password_file.readlines()
+
+# Track whether we have cracked the password
+password_found = False
+
+# Loop through each of the passwords in the file
+# Note:  there is more than one kind of loop we could use here
+for plain_text_password in common_passwords:
+    # Remove the newline character from the line
+    plain_text_password = plain_text_password.strip()
+
+    # Hash the password from the file
+    hashed_password = hashlib.md5(plain_text_password.encode()).hexdigest()
+    # print("Password " + plain_text_password + " hex hash " + hashed_password)
+
+    # Compare the file password to the password we are trying to crack
+    if hashed_password == PASSWORD_TO_CRACK:
+        # If we have cracked the password
+        # print a success message including the cracked password
+        # note that we want to print the plain text not the hash!
+        print("The password is " + plain_text_password)
+        password_found = True
+
+        #    and exit the loop
+        break;
+
+    # otherwise, keep trying new passwords
+    # else:
+    #     print("The password is NOT " + plain_text_password)
+
+if password_found:
+    print("We cracked the password!")
+else:
+    print("We did not crack the password.")
