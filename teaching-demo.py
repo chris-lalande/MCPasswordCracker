cc508c81165673c8de28cfc704f017e2aa4e3d4f
chris-lalande <cplalande@gmail.com>
2022-06-17 10:35:09 -0500

    moving title to variable

--- teaching-demo.py
+++ teaching-demo.py
@@ -10,6 +10,9 @@ ASSIGNED_URL = "http://172.31.29.252"
 MUSIC_URL = "http://davidpots.com/jakeworry/017%20JSON%20Grouping,%20part%203/data.json"
 BOOKS_URL = "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json"
 
+# title to search for
+title_to_search = "Fairy tales"
+
 def parse_json(json_doc):
     if len(json_doc) == 1:
         # if we only got one object back
@@ -19,7 +22,7 @@ def parse_json(json_doc):
         parse_json(json_doc)
     else:
         for media in json_doc:
-            if media['title'] == "Fairy tales":
+            if media['title'] == title_to_search:
                 auth = media['author']
                 return auth
 
