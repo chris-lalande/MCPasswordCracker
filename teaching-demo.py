9ff98dcd4e316202436c182b7a3f5f0a80b3e1ca
chris-lalande <cplalande@gmail.com>
2022-06-17 08:20:29 -0500

    Fixing call to json function

--- teaching-demo.py
+++ teaching-demo.py
@@ -1,13 +1,14 @@
-### json parsing demo
+# json parsing demo
 
-#Library for HTTP requests
+# Library for HTTP requests
 import requests
 
 # Constant to hold our URL
 URL = "http://172.31.29.252"
+MUSIC_URL = "http://davidpots.com/jakeworry/017%20JSON%20Grouping,%20part%203/data.json"
 
-response = requests.get(url=URL)
+response = requests.get(url=MUSIC_URL)
 
-music_and_books = response.json
+music_and_books = response.json()
 
 print(music_and_books)
